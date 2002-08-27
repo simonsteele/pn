da1278ebc18135b6d5e740190d63b1a8b217c79f
Simon Steele <s.steele@pnotepad.org>
2002-08-27 06:41:46 +0000

    *** empty log message ***

--- pnwtl/resource.h
+++ pnwtl/resource.h
@@ -66,7 +66,7 @@
 #ifdef APSTUDIO_INVOKED
 #ifndef APSTUDIO_READONLY_SYMBOLS
 #define _APS_NEXT_RESOURCE_VALUE        205
-#define _APS_NEXT_COMMAND_VALUE         32775
+#define _APS_NEXT_COMMAND_VALUE         32776
 #define _APS_NEXT_CONTROL_VALUE         1005
 #define _APS_NEXT_SYMED_VALUE           104
 #endif
