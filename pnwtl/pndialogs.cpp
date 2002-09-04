419bd344af9454de52472e3232bd3f6e418f498b
Simon Steele <s.steele@pnotepad.org>
2002-09-04 21:30:39 +0000

    Moved implementation into .cpp file and added CPNFileDialog, CPNSaveDialog.

---
+++ pnwtl/pndialogs.cpp
@@ -0,0 +1,161 @@
+/**
+ * @file pndialogs.cpp
+ * @brief Assorted Dialogs for Programmers Notepad 2
+ * @author Simon Steele
+ * @note Copyright (c) 2002 Simon Steele <s.steele@pnotepad.org>
+ *
+ * Programmers Notepad 2 : The license file (license.[txt|html]) describes 
+ * the conditions under which this source may be modified / distributed.
+ */
+
+#include "stdafx.h"
+#include "pndialogs.h"
+
+//////////////////////////////////////////////////////////////////////////////
+// CPNSaveDialog
+//////////////////////////////////////////////////////////////////////////////
+
+CPNSaveDialog::CPNSaveDialog(LPCTSTR szFilter) : baseClass(FALSE, _T(".txt"), NULL, OFN_HIDEREADONLY | OFN_OVERWRITEPROMPT, szFilter)
+{
+	m_ofn.Flags |= OFN_ENABLETEMPLATE;
+	m_ofn.lpTemplateName = MAKEINTRESOURCE (IDD_PNSAVE);
+	//m_ofn.lpstrFilter = _T ("UOAM Map File (*.map)\0*.map\0");
+}
+
+LRESULT CPNSaveDialog::OnInitDialog (UINT /*uMsg*/, WPARAM /*wParam*/, LPARAM /*lParam*/, BOOL &bHandled)
+{
+	m_SaveTypeCombo = GetDlgItem(IDC_PNSAVE_TYPECOMBO);
+	m_SaveTypeLabel = GetDlgItem(IDC_PNSAVE_SAVEASSTATIC);
+
+	CRect rectDlg;
+	GetWindowRect( &rectDlg );
+
+	CRect rectParent;
+	::GetClientRect( GetParent(), &rectParent );
+	rectDlg.right = rectDlg.left + rectParent.Width();
+
+	// Make sure there is enough room at the bottom for resize
+	CRect rectCombo;
+	m_SaveTypeCombo.GetWindowRect( &rectCombo );
+	ScreenToClient( &rectCombo );
+	int cySize = ::GetSystemMetrics( /*SM_CYSIZE*/ SM_CYSIZEFRAME );
+	if (rectDlg.Height() - rectCombo.bottom < cySize)
+		rectDlg.bottom = rectDlg.top + rectCombo.bottom + cySize;
+
+	// Reposition
+	SetWindowPos( NULL, 0, 0, rectDlg.Width(), rectDlg.Height(),
+		SWP_NOMOVE | SWP_NOACTIVATE | SWP_NOZORDER );
+
+	RepositionControls();
+	return TRUE;
+}
+
+LRESULT CPNSaveDialog::OnSize(UINT /*uMsg*/, WPARAM /*wParam*/, LPARAM /*lParam*/, BOOL &bHandled)
+{
+	RepositionControls();
+	bHandled = FALSE;
+	return FALSE;
+}
+
+LRESULT CPNSaveDialog::OnComboSelChange(WORD /*wNotifyCode*/, WORD wID, HWND /*hWndCtl*/, BOOL& /*bHandled*/)
+{
+	// Store the new selection in here...
+
+	return TRUE;
+}
+
+void CPNSaveDialog::RepositionControls()
+{
+	RepositionControl( m_SaveTypeCombo, cmb1, true );
+	RepositionControl( m_SaveTypeLabel, stc2, false );
+
+	RepositionPlacesBar( m_SaveTypeCombo );
+}
+
+void CPNSaveDialog::RepositionPlacesBar(CWindow &bottomwnd)
+{
+	CRect rc, rc2;
+	CWindow wndParent = GetParent();
+	CWindow wndPB = wndParent.GetDlgItem( ctl1 );
+	if( wndPB.IsWindow() )
+	{
+		wndPB.GetWindowRect( &rc );
+		bottomwnd.GetWindowRect( &rc2 );
+		wndParent.ScreenToClient( &rc );
+		ScreenToClient( &rc2 );
+		rc.bottom = rc2.bottom;
+		wndPB.SetWindowPos( NULL, &rc, SWP_NOACTIVATE | SWP_NOZORDER );
+	}
+}
+
+/**
+ * @brief Reposition a control
+ * @param wnd Control to be reposition
+ * @param nID ID of the control used for positioning
+ * @param fSize If true, adjust the width of the control
+ */
+void CPNSaveDialog::RepositionControl(CWindow &wnd, UINT nID, bool fSize)
+{
+	// Get the window rect in the client area of the 
+	// control we are interested in.
+	CWindow wndParent = GetParent();
+	CWindow wndAnchor = wndParent.GetDlgItem( nID );
+	CRect rectAnchor;
+	wndAnchor.GetWindowRect( &rectAnchor );
+	wndParent.ScreenToClient( &rectAnchor );
+
+	//
+	// Reposition the control
+	//
+
+	DWORD dwSWFlags = SWP_NOACTIVATE | SWP_NOZORDER | SWP_NOSIZE;
+	CRect rectCtrl;
+	wnd.GetWindowRect( &rectCtrl );
+	ScreenToClient( &rectCtrl );
+	rectCtrl.OffsetRect( rectAnchor.left - rectCtrl.left, 0 );
+	if( fSize )
+	{
+		rectCtrl.right = rectCtrl.left + rectAnchor.Width();
+		dwSWFlags &= ~SWP_NOSIZE;
+	}
+	wnd.SetWindowPos( NULL, rectCtrl.left, rectCtrl.top,
+		rectCtrl.Width(), rectCtrl.Height(), dwSWFlags );
+	return;
+}
+
+//////////////////////////////////////////////////////////////////////////////
+// CGotoDialog
+//////////////////////////////////////////////////////////////////////////////
+
+LRESULT CGotoDialog::OnInitDialog(UINT /*uMsg*/, WPARAM /*wParam*/, LPARAM /*lParam*/, BOOL& /*bHandled*/)
+{
+	CenterWindow(GetParent());
+	return TRUE;
+}
+
+LRESULT CGotoDialog::OnOK(WORD /*wNotifyCode*/, WORD wID, HWND /*hWndCtl*/, BOOL& /*bHandled*/)
+{
+	HWND hEdit = GetDlgItem(IDC_GOTOEDIT);
+	int i = ::GetWindowTextLength(hEdit);
+	i++;
+	TCHAR *t = new TCHAR[i];
+	::GetWindowText(hEdit, t, i);
+	lineno = _ttoi(t);
+	delete [] t;
+
+	EndDialog(wID);
+
+	return TRUE;
+}
+
+LRESULT CGotoDialog::OnCancel(WORD /*wNotifyCode*/, WORD wID, HWND /*hWndCtl*/, BOOL& /*bHandled*/)
+{
+	EndDialog(wID);
+	
+	return TRUE;
+}
+
+int CGotoDialog::GetLineNo()
+{
+	return lineno;
+}
--- pnwtl/pndialogs.h
+++ pnwtl/pndialogs.h
@@ -10,6 +10,108 @@
 #ifndef pndialogs_h__included
 #define pndialogs_h__included
 
+#include "resource.h"
+
+/**
+ * @class CPNFileDialogImpl
+ * @brief Very simply adds handling for pipe (|) separated filters.
+ */
+template <class T>
+class CPNFileDialogImpl : public CFileDialogImpl<T>
+{
+	public:
+		CPNFileDialogImpl(BOOL bOpenFileDialog, // TRUE for FileOpen, FALSE for FileSaveAs
+			LPCTSTR lpszDefExt = NULL,
+			LPCTSTR lpszFileName = NULL,
+			DWORD dwFlags = OFN_HIDEREADONLY | OFN_OVERWRITEPROMPT,
+			LPCTSTR lpszFilter = NULL,
+			HWND hWndParent = NULL,
+			bool bUsePipeChar = true)
+			: CFileDialogImpl<T>(bOpenFileDialog, lpszDefExt, lpszFileName, dwFlags, lpszFilter, hWndParent)
+		{
+			if(lpszFilter != NULL && bUsePipeChar)
+			{
+				m_szFilter = new TCHAR[_tcslen(lpszFilter)+1];
+				_tcscpy(m_szFilter, lpszFilter);
+				
+				LPTSTR pch = m_szFilter;
+				while ((pch = _tcschr(pch, '|')) != NULL)
+					*pch++ = '\0';
+
+				m_ofn.lpstrFilter = m_szFilter;
+			}
+			else
+			{
+				m_szFilter = NULL;
+				m_ofn.lpstrFilter = lpszFilter;
+			}				
+		}
+
+		~CPNFileDialogImpl()
+		{
+			if(m_szFilter != NULL)
+				delete [] m_szFilter;
+		}
+
+		BEGIN_MSG_MAP(CPNFileDialogImpl)
+		END_MSG_MAP()
+
+	protected:
+		LPTSTR m_szFilter;
+};
+
+class CPNFileDialog : public CPNFileDialogImpl<CPNFileDialog>
+{
+public:
+	CPNFileDialog(BOOL bOpenFileDialog, // TRUE for FileOpen, FALSE for FileSaveAs
+		LPCTSTR lpszDefExt = NULL,
+		LPCTSTR lpszFileName = NULL,
+		DWORD dwFlags = OFN_HIDEREADONLY | OFN_OVERWRITEPROMPT,
+		LPCTSTR lpszFilter = NULL,
+		HWND hWndParent = NULL)
+		: CPNFileDialogImpl<CPNFileDialog>(bOpenFileDialog, lpszDefExt, lpszFileName, dwFlags, lpszFilter, hWndParent)
+	{ }
+
+	// override base class map and references to handlers
+	DECLARE_EMPTY_MSG_MAP()
+};
+
+/**
+ * @class CPNSaveDialog
+ * @brief Save dialog with added controls for save type (e.g. LF, CRLF, etc.)
+ *
+ * Thanks to Tim Smith for his excellent Code Project article which taught me
+ * how to do this without tearing my hair out.
+ * link: http://www.codeproject.com/useritems/uoth.asp
+ */
+class CPNSaveDialog : public CPNFileDialogImpl<CPNSaveDialog>
+{
+	typedef CPNFileDialogImpl<CPNSaveDialog> baseClass;
+	public:
+		CPNSaveDialog(LPCTSTR szFilter);
+
+		BEGIN_MSG_MAP (CPNSaveDialog)
+			MESSAGE_HANDLER(WM_INITDIALOG, OnInitDialog)
+			MESSAGE_HANDLER(WM_SIZE, OnSize)
+
+			COMMAND_HANDLER(IDC_PNSAVE_TYPECOMBO, CBN_SELCHANGE, OnComboSelChange) 
+
+			CHAIN_MSG_MAP (baseClass);
+		END_MSG_MAP ()
+
+	protected:
+		LRESULT OnComboSelChange(WORD /*wNotifyCode*/, WORD wID, HWND /*hWndCtl*/, BOOL& /*bHandled*/);
+		LRESULT OnSize (UINT /*uMsg*/, WPARAM /*wParam*/, LPARAM /*lParam*/, BOOL &bHandled);
+		LRESULT OnInitDialog (UINT /*uMsg*/, WPARAM /*wParam*/, LPARAM /*lParam*/, BOOL &bHandled);
+
+		void RepositionControls();
+		void RepositionControl(CWindow &wnd, UINT nID, bool fSize);
+		void RepositionPlacesBar(CWindow &bottomwnd);
+
+		CComboBox	m_SaveTypeCombo;
+		CStatic		m_SaveTypeLabel;
+};
+
 class CGotoDialog : public CDialogImpl<CGotoDialog>
 {
 	public:
@@ -21,38 +123,13 @@ class CGotoDialog : public CDialogImpl<CGotoDialog>
 			COMMAND_ID_HANDLER(IDCANCEL, OnCancel)
 		END_MSG_MAP()
 
-		LRESULT OnInitDialog(UINT /*uMsg*/, WPARAM /*wParam*/, LPARAM /*lParam*/, BOOL& /*bHandled*/)
-		{
-			CenterWindow(GetParent());
-			return TRUE;
-		}
+		LRESULT OnInitDialog(UINT /*uMsg*/, WPARAM /*wParam*/, LPARAM /*lParam*/, BOOL& /*bHandled*/);
 		
-		LRESULT OnOK(WORD /*wNotifyCode*/, WORD wID, HWND /*hWndCtl*/, BOOL& /*bHandled*/)
-		{
-			HWND hEdit = GetDlgItem(IDC_GOTOEDIT);
-			int i = ::GetWindowTextLength(hEdit);
-			i++;
-			TCHAR *t = new TCHAR[i];
-			::GetWindowText(hEdit, t, i);
-			lineno = _ttoi(t);
-			delete [] t;
+		LRESULT OnOK(WORD /*wNotifyCode*/, WORD wID, HWND /*hWndCtl*/, BOOL& /*bHandled*/);
 
-			EndDialog(wID);
+		LRESULT OnCancel(WORD /*wNotifyCode*/, WORD wID, HWND /*hWndCtl*/, BOOL& /*bHandled*/);
 
-			return TRUE;
-		}
-
-		LRESULT OnCancel(WORD /*wNotifyCode*/, WORD wID, HWND /*hWndCtl*/, BOOL& /*bHandled*/)
-		{
-			EndDialog(wID);
-			
-			return TRUE;
-		}
-
-		int GetLineNo()
-		{
-			return lineno;
-		}
+		int GetLineNo();
 	
 	protected:
 		int lineno;
