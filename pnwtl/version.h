a839c04e3089f9c7c4e0fae62034178c4b085c85
Simon Steele <s.steele@pnotepad.org>
2005-01-25 10:44:37 +0000

    Updated version before branch

--- pnwtl/version.h
+++ pnwtl/version.h
@@ -1,8 +1,8 @@
 #define PN_MAJOR			2
 #define PN_MINOR			0
 #define PN_REVISION			5
-#define PN_BUILD			47
-#define PN_VERSTRING		"2.0.5.47"
+#define PN_BUILD			48
+#define PN_VERSTRING		"2.0.5.48"
 #define PN_VERNAME			"-devel"
 
 #define PN_VIVERSION		PN_MAJOR,PN_MINOR,0,0
