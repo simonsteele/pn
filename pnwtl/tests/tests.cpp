dff0af20e28f86c0f55665f6167f36a06e0f6727
Simon Steele <s.steele@pnotepad.org>
2008-07-16 21:45:30 +0000

    Add missing tests project

---
+++ pnwtl/tests/tests.cpp
@@ -0,0 +1,16 @@
+// tests.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+
+extern void testString();
+extern void testAC();
+
+int _tmain(int argc, _TCHAR* argv[])
+{
+	testString();
+	testAC();
+
+	return 0;
+}
+
