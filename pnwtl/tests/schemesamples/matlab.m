e40bacf173ea4534e6eeb68d2d4f3186cd60f378
Simon Steele <s.steele@pnotepad.org>
2011-01-10 20:39:21 +0000

    Add sample Matlab code.

---
+++ pnwtl/tests/schemesamples/matlab.m
@@ -0,0 +1,36 @@
+﻿% Example Monte Carlo Simulation in Matlab
+% Function: y = x2^2/x1
+%
+%   Generate n samples from a normal distribution
+%     r = ( randn(n,1) * sd ) + mu
+%     mu : mean
+%     sd : standard deviation
+%
+%   Generate n samples from a uniform distribution
+%     r = a + rand(n,1) * (b-a)
+%     a : minimum
+%     b : maximum
+
+n = 100000;	% The number of function evaluations
+
+% --- Generate vectors of random inputs
+% x1 ~ Normal distribution N(mean=100,sd=5)
+% x2 ~ Uniform distribution U(a=5,b=15)
+
+x1 = ( randn(n,1) * 5 ) + 100;
+x2 = 5 + rand(n,1) * ( 15 - 5 );
+
+% --- Run the simulation
+% Note the use of element-wise multiplication
+
+y = x2.^2 ./ x1;
+
+% --- Create a histogram of the results (50 bins)
+
+hist(y,50);
+
+% --- Calculate summary statistics
+
+y_mean = mean(y)
+y_std = std(y)
+y_median = median(y)
