4ac25f33c9936e03c838d9ccb10827accbdd8841
Simon Steele <s.steele@pnotepad.org>
2011-05-01 21:05:11 +0100

    Version changes

--- pnwtl/pypn/pypn.cpp
+++ pnwtl/pypn/pypn.cpp
@@ -67,7 +67,7 @@ bool __stdcall pn_init_extension(int iface_version, extensions::IPN* pn)
 void __declspec(dllexport) __stdcall pn_get_extension_info(PN::BaseString& name, PN::BaseString& version)
 {
 	name = "PyPN";
-	version = "0.14";
+	version = "1.0";
 }
 
 void __declspec(dllexport) __stdcall pn_exit_extension()
